40ae5db6c4521134b646989672dba103e98db148
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2024-12-13 18:23:39 +0530

    Book entity
    add a book entity

---
+++ src/main/java/com/example/Book/Management/System/entity/Book.java
@@ -0,0 +1,27 @@
+package com.example.Book.Management.System.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String title;
+    private String author;
+    private String genre;
+    private Double price;
+    private LocalDateTime publishedDate;
+}
