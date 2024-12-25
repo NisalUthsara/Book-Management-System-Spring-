0fcbc5ee0b2f34764fd8eb374594fb35585ee090
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2024-12-25 22:30:59 +0530

    Create BookController

---
+++ src/main/java/com/example/Book/Management/System/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.Book.Management.System.controller;
+
+import com.example.Book.Management.System.entity.Book;
+import com.example.Book.Management.System.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+    private final BookService bookService;
+    @Autowired
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+    @PostMapping
+    public Book createBook(@RequestBody Book book){
+        return bookService.saveBook(book);
+    }
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id){
+        return bookService.getBookById(id);
+    }
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id,@RequestBody Book updateBook){
+        return bookService.updateBook(id, updateBook);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return "Book with ID "+id+" has been deleted successfully.";
+    }
+}
