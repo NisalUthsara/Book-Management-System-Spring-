7cf34c62fe5840a009c68e740234cec02712547f
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2025-01-17 21:35:31 +0530

    update

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//You can set a base URL or headers globally so you don’t repeat them in every request
 const api = axios.create({
     baseURL: 'http://localhost:8080/api/v2/books', //update the base URL to match my backend
 });
