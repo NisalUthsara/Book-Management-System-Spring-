20d6a4843f77a3443c1b8c9e4cf587278b5d6870
NisalUthsara <117739668+NisalUthsara@users.noreply.github.com>
2025-04-09 14:59:40 +0530

    feature: protected route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,11 +3,16 @@ import Dashboard from "./components/Dashboard";
 import Manage from "./components/Manage";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element:<Dashboard/>,
+        element: (
+            <ProtectedRoute>
+                <Dashboard/>
+            </ProtectedRoute>
+        ),
         children: [
             {
                 index:true, //this makes it the default route
